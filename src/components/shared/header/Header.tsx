ed399d99d96b9b66557dbaa9cb1c1d2fd40dc0df
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-01-31 19:51:25 +0200

    added burger menu

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,13 +1,22 @@
+"use client";
+import { useState } from "react";
 import Container from "../container/Container";
 import LogoLink from "../logoLink/LogoLink";
 import NavMenu from "./NavMenu";
+import BurgerMenu from "./burgerMenu/BurgerMenu";
 
 export default function Header() {
+  const [isOpenBurgerMenu, setIsOpenBurgerMenu] = useState(false);
+
   return (
     <header>
       <Container className="flex py-4 lg:py-11 justify-between lg:justify-start items-center lg:gap-[228px]">
         <LogoLink />
         <NavMenu />
+        <BurgerMenu
+          isOpenBurgerMenu={isOpenBurgerMenu}
+          setIsOpenBurgerMenu={setIsOpenBurgerMenu}
+        />
       </Container>
     </header>
   );
