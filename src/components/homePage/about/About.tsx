a9dd9e10b1ced25696610ea054e01b0daab77e32
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-02 18:51:05 +0200

    added about section

---
+++ src/components/homePage/about/About.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import SecondaryButton from "@/components/shared/buttons/SecondaryButton";
+
+export default function About() {
+  return (
+    <section className="py-12 lg:pt-[104px] lg:pb-0 text-white">
+      <Container>
+        <div className="relative rounded-[18px] px-3 lg:px-[46px] pt-8 lg:pt-[62px] pb-[182px] lg:pb-[117px]">
+          <div className="absolute -z-10 inset-0 overflow-hidden rounded-[18px] bg-[linear-gradient(353.3deg,rgba(0,0,0,0)_44.74%,rgba(0,0,0,0.6)_67.83%)]">
+            <Image
+              src="/images/homePage/about/about.webp"
+              alt="About CO2LAB"
+              fill
+              className="object-cover -z-10"
+            />
+            <div className="absolute z-10 inset-0 overflow-hidden rounded-[18px] bg-[linear-gradient(353.3deg,rgba(0,0,0,0)_44.74%,rgba(0,0,0,0.6)_67.83%)]" />
+            <div className="absolute top-[-131px] lg:top-[-537px] right-0 z-20 w-[232px] lg:w-[538px] h-[259px] lg:h-[1068px]">
+              <Image
+                src="/images/homePage/about/smoke.webp"
+                alt="Smoke"
+                fill
+                className="object-cover"
+                aria-hidden
+              />
+            </div>
+          </div>
+          <SectionTitle className="mb-3 lg:mb-2 text-white xl:text-[64px]">
+            ABOUT
+            <br />
+            CO2LAB
+          </SectionTitle>
+          <p className="sm:max-w-[431px] mb-8 text-[12px] lg:text-[16px] font-light leading-[120%]">
+            At CO₂ Lab, we transform emissions into innovation. By closing the
+            carbon loop, we give new purpose to what was once waste — creating
+            technologies that drive a sustainable future.
+          </p>
+          <Link href="/about">
+            <SecondaryButton>Read more</SecondaryButton>
+          </Link>
+        </div>
+      </Container>
+    </section>
+  );
+}
---
+++ src/components/shared/buttons/SecondaryButton.tsx
@@ -0,0 +1,43 @@
+import ArrowIcon from "../icons/ArrowIcon";
+import { twMerge } from "tailwind-merge";
+
+interface SecondaryButtonProps {
+  children: React.ReactNode;
+  type?: "button" | "submit";
+  className?: string;
+  variant?: "black" | "white";
+  onClick?: () => void;
+}
+
+export default function SecondaryButton({
+  type = "button",
+  children,
+  className,
+  variant = "white",
+  onClick,
+}: SecondaryButtonProps) {
+  const isBlack = variant === "black";
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={twMerge(
+        "group flex items-center h-11 lg:h-[54px] rounded-full cursor-pointer active:scale-[98%] will-change-transform transition duration-300 ease-in-out focus-visible:brightness-125 xl:hover:brightness-125",
+        "text-[12px] lg:text-[14px] font-normal leading-[120%]",
+        className,
+      )}
+    >
+      <span
+        className={`flex items-center justify-center h-full px-7 lg:px-9 py-3 rounded-full text-[12px] lg:text-[14px] font-normal leading-[120%] ${isBlack ? "bg-black text-white" : "bg-white text-black"}`}
+      >
+        {children}
+      </span>
+      <span
+        className={`flex items-center justify-center h-full aspect-square py-3 rounded-full ${isBlack ? "bg-black text-white" : "bg-white text-black"}`}
+      >
+        <ArrowIcon className="h-[14px] w-[17px] shrink-0 transition-transform duration-300 ease-in-out group-focus-visible:translate-x-[2px] group-focus-visible:-translate-y-[2px] xl:group-hover:translate-x-[2px] xl:group-hover:-translate-y-[2px]" />
+      </span>
+    </button>
+  );
+}
