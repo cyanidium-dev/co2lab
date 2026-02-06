6589ac667b5b37248f1f732a1f6daa3c28eddfe9
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-06 13:58:32 +0200

    updated scroll header effect for mobile

--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -21,6 +21,27 @@ export default function Header() {
   return (
     <header className="fixed left-0 right-0 top-0 z-50 py-4 lg:py-11">
       <Container className="relative z-10 flex items-center justify-between rounded-full lg:justify-start lg:gap-[228px]">
+        {scrollPosition > 20 && (
+          <>
+            <div
+              className="md:hidden absolute w-[calc(100%-8px)] h-full top-0 left-1 px-4 -z-10 rounded-full bg-[linear-gradient(90.95deg,rgba(231,231,231,0.8)_52.25%,rgba(255,255,255,0.8)_99.18%)] shadow-[inset_0px_4px_12.6px_0px_rgba(255,255,255,0.25)] backdrop-blur-[10px]"
+              aria-hidden
+            />
+            <div
+              className="md:hidden absolute w-[calc(100%-8px)] h-full top-0 left-1 -z-10 rounded-full pointer-events-none"
+              style={{
+                background:
+                  "linear-gradient(270.67deg, #F2F2F2 -9.58%, #C7C7C7 103.45%)",
+                padding: "1px",
+                WebkitMask:
+                  "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+                WebkitMaskComposite: "xor",
+                maskComposite: "exclude",
+              }}
+              aria-hidden
+            />
+          </>
+        )}
         <div className="relative py-2">
           <LogoLink onNavigate={() => setIsOpenBurgerMenu(false)} />
           {scrollPosition > 20 && (
