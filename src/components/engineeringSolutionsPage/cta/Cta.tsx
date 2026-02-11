65d9600f693ff1b82f0d20196cf43481c1a90452
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-11 19:43:20 +0200

    added smoked images

--- src/components/engineeringSolutionsPage/cta/Cta.tsx
+++ src/components/engineeringSolutionsPage/cta/Cta.tsx
@@ -14,9 +14,24 @@ export default function Cta() {
             fill
             className="object-cover -z-20"
           />
+          <Image
+            src="/images/engineeringSolutionsPage/cta/smokeMob.webp"
+            alt=""
+            width={338}
+            height={524}
+            className="pointer-events-none absolute right-0 -bottom-62 z-0 lg:hidden object-cover"
+            aria-hidden
+          />
+          <Image
+            src="/images/engineeringSolutionsPage/cta/smokeDesk.webp"
+            alt=""
+            width={636}
+            height={436}
+            className="pointer-events-none absolute right-0 bottom-0 z-0 object-contain hidden lg:block"
+            aria-hidden
+          />
           <div className="lg:hidden absolute inset-0 -z-10 opacity-60 bg-[linear-gradient(0deg,rgba(38,38,38,0.4),rgba(38,38,38,0.4)),linear-gradient(70.75deg,rgba(0,0,0,0)_6.59%,rgba(0,0,0,0.8)_86.62%)]"></div>
           <div className="lg:max-w-[604px]">
-            {" "}
             <SectionTitle className="mb-3 lg:mb-8">
               Ready to build your CO₂ ecosystem?
             </SectionTitle>
