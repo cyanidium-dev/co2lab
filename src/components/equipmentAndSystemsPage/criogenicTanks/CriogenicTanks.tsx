d1f1879de7057104d8b818449955356041519f12
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-12 16:55:15 +0200

    added criogenic tanks

---
+++ src/components/equipmentAndSystemsPage/criogenicTanks/CriogenicTanks.tsx
@@ -0,0 +1,36 @@
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import Image from "next/image";
+
+export default function CriogenicTanks() {
+  return (
+    <section className="py-12 lg:pt-[142px] lg:pb-0">
+      <Container>
+        <div className="flex flex-col sm:flex-row-reverse sm:items-center gap-8 mb-8 lg:mb-12">
+          <div className="sm:w-[calc(50%-16px)] ">
+            <SectionTitle className="mb-3 lg:mb-4.5 lg:text-[44px] xl:text-[48px]">
+              Cryogenic Tanks and Equipment from CO₂ Lab
+            </SectionTitle>
+            <p className="mb-3 lg:mb-4.5">
+              CO₂ Lab Engineering Bureau offers a wide range of cryogenic
+              equipment for the storage and transportation of liquefied gases
+              such as CO₂, oxygen (LOX), nitrogen (LIN), argon (LAr), natural
+              gas (LNG), and other industrial gases.
+            </p>
+            <p>
+              Our cryogenic tanks meet modern safety and efficiency standards.
+            </p>
+          </div>
+          <div className="relative sm:w-[calc(50%-16px)] h-60 xs:h-80 sm:h-70 lg:h-[435px] rounded-[18px] overflow-hidden">
+            <Image
+              src="/images/equipmentAndSystemsPage/criogenicTanks/image.webp"
+              alt="Processing"
+              fill
+              className="object-cover object-[75%_20%]"
+            />
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
