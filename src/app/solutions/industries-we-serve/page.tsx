5daf4e02fe7ee8e64ed35431b3a54b530658ba73
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-13 15:15:22 +0200

    added hero section

--- src/app/solutions/industries-we-serve/page.tsx
+++ src/app/solutions/industries-we-serve/page.tsx
@@ -1,8 +1,10 @@
 import SupportCTA from "@/components/shared/cta/SupportCTA";
+import Hero from "@/components/industriesWeServePage/hero/Hero";
 
 export default function IndustriesWeServe() {
   return (
     <>
+      <Hero />
       <SupportCTA />
     </>
   );
