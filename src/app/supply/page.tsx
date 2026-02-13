a47bff68bcc2757b24d7c294aa0709cd5913fee7
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-13 10:55:43 +0200

    added cta to the pages

--- src/app/supply/page.tsx
+++ src/app/supply/page.tsx
@@ -1,3 +1,9 @@
+import ConsultationCTA from "@/components/shared/cta/ConsultationCTA";
+
 export default function SupplyPage() {
-  return <></>;
+  return (
+    <>
+      <ConsultationCTA />
+    </>
+  );
 }
