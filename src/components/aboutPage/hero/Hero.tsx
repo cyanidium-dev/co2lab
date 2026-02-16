171167f562266f3acf1ac57bc26beeddfc8aa407
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-16 15:12:10 +0200

    added hero

---
+++ src/components/aboutPage/hero/Hero.tsx
@@ -0,0 +1,62 @@
+import Container from "@/components/shared/container/Container";
+import MainButton from "@/components/shared/buttons/MainButton";
+import PageTitle from "@/components/shared/titles/PageTitle";
+import Image from "next/image";
+import Badges from "./Badges";
+
+export default function Hero() {
+  return (
+    <section className="pt-22 pb-4 lg:pt-8 lg:pb-0">
+      <Container className="flex flex-col gap-4 lg:flex-row lg:gap-8">
+        <div className="lg:w-[calc(50%-16px)] lg:pt-30 lg:pb-10">
+          <PageTitle className="mb-3 lg:mb-5.5 max-w-[160px]">
+            About CO₂ Lab
+          </PageTitle>
+          <p className="mb-4 lg:mb-10">
+            CO₂ Lab delivers end-to-end CO₂ technologies — from capture and
+            purification to supply and logistics — for industrial partners
+            worldwide.
+          </p>
+
+          <MainButton className="sm:max-w-[288px] lg:mb-20">
+            Contact Us
+          </MainButton>
+
+          <Badges className="hidden lg:flex lg:max-w-[335px]" />
+        </div>
+        <div className="relative w-full overflow-hidden lg:w-[calc(50%-16px)] min-h-[305px] lg:min-h-[420px] rounded-[18px]">
+          <Image
+            src="/images/homePage/hero/bgMobile.svg"
+            alt=""
+            width={152}
+            height={190}
+            className="absolute -z-10 right-[-41px] top-[-74px] lg:hidden"
+            aria-hidden
+          />
+          <Image
+            src="/images/homePage/hero/bgDesk.svg"
+            alt=""
+            width={269}
+            height={338}
+            className="absolute -z-10 bottom-[-163px] left-[-164px] hidden lg:block"
+            aria-hidden
+          />
+          <Image
+            src="/images/aboutPage/hero/hero.webp"
+            alt=""
+            fill
+            priority
+            fetchPriority="high"
+            className="object-cover rounded-[18px]"
+            sizes="(max-width: 1024px) 100vw, 50vw"
+          />
+          <div
+            className="lg:hidden absolute inset-0 z-10 rounded-[18px] bg-[linear-gradient(176.8deg,rgba(0,0,0,0)_59.28%,rgba(0,0,0,0.6)_79.3%)]"
+            aria-hidden
+          />
+        </div>
+        <Badges className="lg:hidden" />
+      </Container>
+    </section>
+  );
+}
