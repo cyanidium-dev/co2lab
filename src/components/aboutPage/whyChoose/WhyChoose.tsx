131b0d0611a1b3a05a0fc7640e598579af78d9b4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-16 19:02:36 +0200

    added why choose section

---
+++ src/components/aboutPage/whyChoose/WhyChoose.tsx
@@ -0,0 +1,48 @@
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import Image from "next/image";
+
+export default function WhyChoose() {
+  return (
+    <section className="py-12 lg:pt-30 lg:pb-0">
+      <Container className="relative flex flex-col md:flex-row-reverse md:items-center gap-8 md:gap-5 lg:gap-15 xl:gap-[98px]">
+        <Image
+          src="/images/aboutPage/whyChoose/bgMob.svg"
+          alt=""
+          aria-hidden
+          className="absolute -z-10 -top-29 -right-6 pointer-events-none lg:hidden"
+          width="208"
+          height="213"
+        />
+        <Image
+          src="/images/aboutPage/whyChoose/bgDesk.svg"
+          alt=""
+          aria-hidden
+          className="absolute -z-10 -top-25 -right-3 pointer-events-none hidden lg:block"
+          width="283"
+          height="274"
+        />
+        <div className="md:w-[calc(50%-10px)]">
+          <SectionTitle className="mb-3 lg:mb-4.5">
+            <span className="block">Why Choose</span>
+            <span className="block pl-[150px] lg:pl-[250px]">CO₂ Lab</span>
+          </SectionTitle>
+          <p>
+            CO₂ Lab stands out by combining deep technological knowledge with
+            practical, reliable delivery across industries. Whether it’s
+            production, logistics, or tailored CO₂ applications, our team
+            ensures performance and partnership.
+          </p>
+        </div>
+        <div className="relative rounded-[18px] md:w-[calc(50%-10px)] h-[158px] xs:h-[248px] md:h-auto md:min-h-[208px] lg:min-h-[281px] overflow-hidden">
+          <Image
+            src="/images/aboutPage/whyChoose/whyChoose.webp"
+            alt="Why Choose CO₂ Lab"
+            fill
+            className="object-cover object-[50%_70%]"
+          />
+        </div>
+      </Container>
+    </section>
+  );
+}
