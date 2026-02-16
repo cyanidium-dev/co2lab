fa1579dbe99e655456fff4ff05e44022eb5e639e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-16 19:17:14 +0200

    added cta section

---
+++ src/components/shared/cta/BuildCTA.tsx
@@ -0,0 +1,65 @@
+import SecondaryButton from "@/components/shared/buttons/SecondaryButton";
+import Container from "@/components/shared/container/Container";
+import SectionTitle from "@/components/shared/titles/SectionTitle";
+import Image from "next/image";
+
+export default function BuildCTA() {
+  return (
+    <section className="pt-12 pb-24 lg:pt-[127px] lg:pb-[108px] text-white">
+      <Container className="relative">
+        <Image
+          src="/images/cta/bgMob.svg"
+          alt=""
+          width={200}
+          height={240}
+          className="absolute -top-12 right-9 -z-20 pointer-events-none lg:hidden w-auto h-auto"
+          aria-hidden
+        />
+        <Image
+          src="/images/cta/bgDesk.svg"
+          alt=""
+          width={320}
+          height={380}
+          className="absolute -top-24 right-12 -z-20 pointer-events-none hidden lg:block w-auto h-auto"
+          aria-hidden
+        />
+        <div className="relative px-3 lg:px-12 pt-8 pb-[178px] sm:pb-15 lg:py-[95px] rounded-[18px] overflow-hidden">
+          <Image
+            src="/images/cta/build.webp"
+            alt="Cta"
+            fill
+            className="object-cover -z-20"
+          />
+
+          <Image
+            src="/images/cta/smokeMob.webp"
+            alt="smoke"
+            width={338}
+            height={524}
+            className="pointer-events-none absolute right-0 -bottom-62 z-0 lg:hidden object-cover"
+            aria-hidden
+          />
+          <Image
+            src="/images/cta/smokeDeskBuild.webp"
+            alt="smoke"
+            width={728}
+            height={464}
+            className="pointer-events-none absolute right-0 bottom-0 z-0 object-contain hidden lg:block"
+            aria-hidden
+          />
+          <div className="lg:hidden absolute inset-0 -z-10 opacity-60 bg-[linear-gradient(0deg,rgba(38,38,38,0.4),rgba(38,38,38,0.4)),linear-gradient(70.75deg,rgba(0,0,0,0)_6.59%,rgba(0,0,0,0.8)_86.62%)]"></div>
+          <div className="relative z-10 lg:max-w-[604px]">
+            <SectionTitle className="max-w-[548px] lg:max-w-full mb-3 lg:mb-8">
+              Let’s Build Together
+            </SectionTitle>
+            <p className="max-w-[548px] mb-8 lg:mb-12 text-[12px] lg:text-[18px] font-light leading-[120%]">
+              Tell us about your needs — we’ll craft a CO₂ solution tailored to
+              your operations.
+            </p>
+            <SecondaryButton>Contact Us</SecondaryButton>
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+}
