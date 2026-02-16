668738566219b23bf34a29ae368c8e9e1f9d6119
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-16 17:54:02 +0200

    added bg images

--- src/components/aboutPage/whatWeDo/WhatWeDo.tsx
+++ src/components/aboutPage/whatWeDo/WhatWeDo.tsx
@@ -10,33 +10,33 @@ export default function WhatWeDo() {
       <div className="relative rounded-[28px] bg-black py-8 lg:pt-[59px] lg:pb-12">
         <Container className="relative">
           <Image
-            src="/images/homePage/activity/bgMob.svg"
-            alt=""
-            width={185}
-            height={161}
-            className="absolute -right-1 -top-1 pointer-events-none lg:hidden"
+            src="/images/aboutPage/whatWeDo/bgMob.svg"
+            alt="background mobile"
+            width="188"
+            height="158"
+            className="absolute -right-3 top-0 pointer-events-none lg:hidden"
             aria-hidden
           />
           <Image
-            src="/images/homePage/activity/bgLeftDesk.svg"
-            alt=""
-            width={328}
-            height={285}
-            className="absolute right-[335px] top-5 pointer-events-none hidden lg:block"
+            src="/images/aboutPage/whatWeDo/bgLeftDesk.svg"
+            alt="background left desktop"
+            width="328"
+            height="285"
+            className="absolute right-[447px] xl:right-[647px] -top-5 pointer-events-none hidden lg:block"
             aria-hidden
           />
           <Image
-            src="/images/homePage/activity/bgRightDesk.svg"
-            alt=""
-            width={290}
-            height={322}
-            className="absolute -right-18 top-[69px] pointer-events-none hidden lg:block"
+            src="/images/aboutPage/whatWeDo/bgRightDesk.svg"
+            alt="background right desktop"
+            width="264"
+            height="339"
+            className="absolute right-10 xl:right-30 -top-5 pointer-events-none hidden lg:block"
             aria-hidden
           />
           <SectionTitle className="mb-3 text-white lg:mb-6">
             What We Do
           </SectionTitle>
-          <p className="mb-8 lg:mb-[73px] text-white max-w-[277px]">
+          <p className="mb-8 lg:mb-[73px] text-white max-w-[240px] lg:max-w-[345px] text-[12px] lg:text-[14px] font-light leading-[120%]">
             Our work spans the full lifecycle of industrial CO₂:
           </p>
           <div className="mt-6 lg:mt-8">
