3714cdfe064b3e3be96bf222d365a8a2f7601bce
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-18 11:27:57 +0200

    added request consultation application

---
+++ src/components/shared/cta/RequestConsultation.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import SecondaryButton from "@/components/shared/buttons/SecondaryButton";
+import SendMessageModal from "@/components/shared/modals/SendMessageModal";
+import Backdrop from "@/components/shared/backdrop/Backdrop";
+
+export default function RequestDetailsApplication() {
+  const [isModalShown, setIsModalShown] = useState(false);
+  return (
+    <>
+      <SecondaryButton onClick={() => setIsModalShown(true)}>
+        Request consultation
+      </SecondaryButton>
+      <SendMessageModal
+        isModalShown={isModalShown}
+        setIsModalShown={setIsModalShown}
+      />
+      <Backdrop
+        isVisible={isModalShown}
+        onClick={() => setIsModalShown(false)}
+      />
+    </>
+  );
+}
