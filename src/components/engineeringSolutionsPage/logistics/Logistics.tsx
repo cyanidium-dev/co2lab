7a0df0a5e382ac94a41521218186f9a5d096ef73
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2026-02-19 11:38:26 +0200

    updated styles

--- src/components/engineeringSolutionsPage/logistics/Logistics.tsx
+++ src/components/engineeringSolutionsPage/logistics/Logistics.tsx
@@ -1,10 +1,11 @@
 import Container from "@/components/shared/container/Container";
+import ImageCarousel from "@/components/shared/carousel/ImageCarousel";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
 
 export default function Logistics() {
   return (
     <section className="py-12 lg:pt-30 lg:pb-0">
-      <Container>
+      <Container className="flex flex-col md:flex-row gap-88">
         <SectionTitle className="mb-3 lg:mb-4.5">
           <span className="block">Storage &</span>
           <span className="block pl-[113px] lg:pl-[186px]">Logistics</span>
@@ -12,6 +13,7 @@ export default function Logistics() {
         <p className="lg:max-w-[440px]">
           Safe, efficient and scalable CO₂ infrastructure for any application.
         </p>
+        <ImageCarousel />
       </Container>
     </section>
   );
